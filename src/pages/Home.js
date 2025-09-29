6a2531351416597c0b2044335b2bc6049336908d
Dawnmie <yaomian.ym@outlook.com>
2025-09-29 16:33:42 +0800

    添加主页组件

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-3xl font-bold mb-4">欢迎来到福州</h2>
+      <p>福州是一个充满历史和文化的城市，有许多的旅游景点和美食。</p>
+    </div>
+  );
+};
+
+export default Home;
